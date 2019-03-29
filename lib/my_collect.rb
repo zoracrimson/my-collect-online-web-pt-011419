09d58c0e179dad5c376386de5280ceadb78ff962
Kat Maldon <katharinamaldon@gmail.com>
2019-03-29 15:57:04 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,13 @@
 def my_collect(array)
   i = 0 
   new_array = []
-  while i < array.length 
-    new_array << yield(array[i])
-    i += 1 
-  end
-collection
+  if i > 0
+    while i < array.length 
+      new_array << yield(array[i])
+     i += 1 
+    end
+  else 
+    "Empty array."
+    
+  collection
 end 
